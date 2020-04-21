bd5fab76854fda4b624b8261410721b7476bc7cf
Aris <aris.miletis@icloud.com>
2020-04-21 20:59:08 +0300

    start

--- IphoneCrawler/IphoneCrawler/keyword_extractor.py
+++ IphoneCrawler/IphoneCrawler/keyword_extractor.py
@@ -1,15 +1,19 @@
 import re
 
 import pandas
+from sklearn.feature_extraction.text import CountVectorizer
+
+# nltk.download('stopwords')
+# nltk.download('wordnet')
 
 # load the dataset
 dataset = pandas.read_csv('iphone.csv')
 dataset.head()
 
 # words to remove from dataset
-noise_words = ["iphone", "με", "apple"]
+noise_words = ["iphone", "apple", "i", "phone"]
 stop_words = set(noise_words)
-
+corpus = []
 # get price
 dataset['price_normalized'] = dataset['price'].apply(lambda x: str(x).split(",")[0])
 # measure word count
@@ -18,9 +22,9 @@ dataset['word_count'] = dataset['name'].apply(lambda x: len(str(x).split(" ")))
 frequent_words = pandas.Series(' '.join(dataset['name']).split()).value_counts()[:20]
 rare = pandas.Series(' '.join(dataset['name']).split()).value_counts()[-20:]
 
-for i in range(0, 109):
+for i in range(len(dataset)):
     # Remove punctuations
-    text = re.sub('[^a-zA-Z]', ' ', dataset['name'][i])
+    text = dataset['name'][i]
 
     # Convert to lowercase
     text = text.lower()
@@ -29,7 +33,20 @@ for i in range(0, 109):
     text = re.sub("&lt;/?.*?&gt;", " &lt;&gt; ", text)
 
     # remove special characters and digits
-    text = re.sub("(\\d|\\W)+", " ", text)
-    print(text)
-    ##Convert to list from string
+    text = re.sub("[^a-z0-9]+", " ", text)
+
+    # Convert to list from string
     text = text.split()
+    line = []
+    # Stemming
+    for word in text:
+        if not word in stop_words:
+            line.append(word)
+    text = line
+
+    text = " ".join(text)
+    corpus.append(text)
+
+cv = CountVectorizer(max_df=0.7, stop_words=stop_words, max_features=10000, ngram_range=(1, 1))
+X = cv.fit_transform(corpus)
+print(list(cv.vocabulary_.keys())[:10])
--- IphoneCrawler/IphoneCrawler/settings.py
+++ IphoneCrawler/IphoneCrawler/settings.py
@@ -28,7 +28,7 @@ ROBOTSTXT_OBEY = True
 # See also autothrottle settings and docs
 DOWNLOAD_DELAY = 10
 
-DEPTH_LIMIT = 4
+DEPTH_LIMIT = 10
 # The download delay setting will honor only one of:
 # CONCURRENT_REQUESTS_PER_DOMAIN = 16
 # CONCURRENT_REQUESTS_PER_IP = 16
@@ -71,7 +71,7 @@ ITEM_PIPELINES = {
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_ENABLED = False
 # The initial download delay
 THROTTLE_START_DELAY = 10
 # The maximum download delay to be set in case of high latencies
